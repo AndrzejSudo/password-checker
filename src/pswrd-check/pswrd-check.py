a733bddd087df898052f4d89b80a97b111e5aff6
Andrzej <jk2rlz@gmail.com>
2022-06-13 15:25:43 +0200

    first working release

--- src/pswrd-check/pswrd-check.py
+++ src/pswrd-check/pswrd-check.py
@@ -9,42 +9,54 @@ except ModuleNotFoundError as e:
 finally:
     print("All modules imported")
 
-API_url = "https://api.pwnedpasswords.com/range/"
+API_url = "https://api.pwnedpasswords.com/range/"                          #more info on how it works https://haveibeenpwned.com/API/v3#PwnedPasswords
+
+def main():
+    forbidden_symbols = ['<','>','(',')','[',']','/','\\','~','"','\'',' ',';']
+    while True:
+        pswrd = input("Enter password to check: ")  
+        for symbol in pswrd:
+            if symbol in forbidden_symbols:
+                print('Improper symbol used in password, try again')
+                continue
+            else:
+                call_API(pswrd)
 
 def call_API(pswrd):
-    pswrd_hash = hashlib.sha1(pswrd.encode(), usedforsecurity=True)    #hashing password with sha1
-    pswrd_hash_dig = pswrd_hash.hexdigest().upper()[5:]                #extracting password digest first 5 letters for api
-    API_call = API_url + pswrd_hash_dig                              #haveibeenpwned verifies only 5 first letter of hashed password
-    resp_url = requests.get(API_call)                                       #API response
-    
-    if ('200' in str(resp_url)):                                            #http response: 200 - ok, 400 - not ok
-        pswrd_cnt = 0
-        resp_url_split = resp_url.text.splitlines()
-        for hash in resp_url_split:
-            hash_list = hash.split(':')
-            for hash_line in hash_list:
-                hash_split = hash_line[0]
-                if str(pswrd_hash) in hash_split[5:]:
-                    pswrd_cnt += 1
-        resp_url_split = resp_url.text.splitlines()
-        """
-        for hash in resp_url_split:
-            hash_rec = hash.split(':')[0]
-            hash_cnt = hash.split(':')[1]
-            if hash_rec == str(pswrd_hash)[5:]:
-                    pswrd_cnt += int(hash_cnt)
-        """
-        """
-        hashes = (line.split(':') for line in resp_url.text.splitlines())
-        pswrd_cnt = next((int(pswrd_cnt) for t, pswrd_cnt in hashes if t == pswrd_hash_dig[5:]), 0)"""
-        print(f"You password was compromised {pswrd_cnt} times")
+    while True:
+        pswrd_hash = hashlib.sha1(pswrd.encode(), usedforsecurity=True)    #hashing password with sha1
+        pswrd_hash_dig = pswrd_hash.hexdigest().upper()                    #extracting password digest first 5 letters for api
+        pswrd_hash_dig_prefix = pswrd_hash_dig[:5]
+        API_call = API_url + pswrd_hash_dig_prefix                         #haveibeenpwned verifies only 5 first letter of hashed password
+        resp_url = requests.get(API_call)                                  #API response
         
-    elif ('400' in str(resp_url)):
-        print('Invalid input, try again')
-        call_API(pswrd)
+        if ('200' in str(resp_url)):                                       #http response: 200 - ok, 400 - not ok
+            pswrd_cnt = 0
+            resp_url_split = resp_url.text.splitlines()
 
-    else:
-        print("No idea, try debugging")
-        sys.exit()
-    
-call_API("password")
+            for hashes in resp_url_split:
+                hash = hashes.split(':')
+                if hash[0] == pswrd_hash_dig[5:]:
+                    pswrd_cnt += int(hash[1])
+            print(f"Your password was found {pswrd_cnt} times in leaked databases")
+            while True:
+                response = input("Do you want to try another password? [Y\\N]\n")
+                if response.upper() == 'Y':
+                    main()
+                elif response.upper() == 'N':
+                    print('Bye')
+                    sys.exit()
+                else:
+                    print("Unrecognized input, try again")
+                    continue
+            
+        elif ('400' in str(resp_url)):
+            print('Invalid input, try again')
+            break
+
+        else:
+            print("No idea, try debugging")
+            sys.exit()
+
+if __name__ == '__main__':
+    main()
