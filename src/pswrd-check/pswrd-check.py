427f770953ae4fa1a611e1d47a79f78b183ef3d8
Andrzej <jk2rlz@gmail.com>
2022-08-08 14:59:45 +0200

    finally final

--- src/pswrd-check/pswrd-check.py
+++ src/pswrd-check/pswrd-check.py
@@ -13,15 +13,33 @@ API_url = "https://api.pwnedpasswords.com/range/"                          #more
 arguments = sys.argv[1:]
 forbidden_symbols = ['<','>','(',')','[',']','/','\\','~','"','\'',' ',';']
 
-try:
-    modes, args = getopt.getopt(arguments, "h:p:w:", ["mode1=", "mode2=", "mode3="])
-except:
-    print("invalid argument")
+def mode_help():
+    print("""
+    usage: pswrd-check.py [-h] [-w] [-p] <INPUT>
+    Checks password against given wordlist or via haveibeenpwned api
+
+    positional arguments:
+        INPUT               -   either password or wordlist, mode dependend
+    
+    options:
+        -h, --help          -   displays help info
+        -w, --wordlist      -   wordlists mode, requires password list file to check typed password f.e -w <wordlist.txt> -p <password>
+        -p, --password      -   default mode, checks password leaks via haveibeenpwned api
+    """)
     sys.exit()
+
+try:
+    modes, args = getopt.getopt(arguments, "p:w:h:")
+except:
+    mode_help()
 
 def main():
     while True:
-        pswrd = modes[0][1]
+        try:
+            pswrd = modes[0][1]
+        except IndexError as e:
+            print("Choose mode before typing password\n")
+            mode_help()
         for symbol in pswrd:
             if symbol in forbidden_symbols:
                 print('Improper symbol used in password, try again')
@@ -32,13 +50,13 @@ def main():
                         mode_list(val)
                         sys.exit()
                     elif name in ["-p", "--password"]:
-                        mode_API(val)
+                        mode_web(val)
                     elif name in ["-h", "--help"]:
                         mode_help()
                     else:
                         print("Unrecognized input try -h or --help for more info")
 
-def mode_API(pswrd):
+def mode_web(pswrd):
     while True:
         pswrd_hash = hashlib.sha1(pswrd.encode(), usedforsecurity=True)    #hashing password with sha1
         pswrd_hash_dig = pswrd_hash.hexdigest().upper()                    #extracting password digest first 5 letters
@@ -62,34 +80,14 @@ def mode_API(pswrd):
             print("No idea, check if service is online")
 
 def mode_list(wordlist, *pswrd):
+    if pswrd:
+        val = pswrd[0]
+        wordlist_load(wordlist, val)
     for name, val in modes:
-        if name in ["-p", "--password"] or pswrd:
-            with open(wordlist, "r") as file:
-                passwords = (file.read().split(" "))
-                for password in passwords:
-                    if val == password or pswrd == password:
-                        print("Password compromised")
-                        file.close()
-                        sys.exit()
-                print("Password not found. You're good.")
-                file.close()
-                sys.exit()
+        if name in ["-p", "--password"]:
+                wordlist_load(wordlist, val)
     print("Missing password argument")
     print("Try like this: -w <wordlist> -p <password>")
-
-def mode_help():
-    print("""
-    usage: pswrd-check.py [-h] [-w] [-p] <INPUT>
-    Checks password against given wordlist or via haveibeenpwned api
-
-    positional arguments:
-        INPUT               -   either password or wordlist, mode dependend
-    
-    options:
-        -h, --help          -   displays help info
-        -w, --wordlist      -   wordlists mode, requires password list file to check typed password f.e -w <wordlist.txt> -p <password>
-        -p, --password      -   default mode, checks password leaks via haveibeenpwned api
-    """)
 
 def mode_picker():
     while True:
@@ -112,6 +110,18 @@ def mode_picker():
         else:
             print("Use only numbers to choose mode '1' or '2'")
             continue
+
+def wordlist_load(wordlist, val):
+    with open(wordlist, "r") as file:
+        wordlist_split = (file.read().split(" "))
+    for password in wordlist_split:
+            if val == password:
+                print("Password compromised")
+                file.close()
+                sys.exit()
+    print("Password not found. You're good.")
+    file.close()
+    sys.exit()    
 
 if __name__ == '__main__':
     if arguments:
